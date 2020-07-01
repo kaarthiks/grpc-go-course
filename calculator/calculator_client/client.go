9b03d52b5c5c31bc9ea440658e64f52c962771c9
Kaarthik Sivakumar <8054726+kaarthiks@users.noreply.github.com>
2020-07-01 14:17:18 +0530

    Prime factor decomposition as server streaming.
    Signed-off-by: Kaarthik Sivakumar <8054726+kaarthiks@users.noreply.github.com>

--- calculator/calculator_client/client.go
+++ calculator/calculator_client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/kaarthiks/grpc-go-course/calculator/calculatorpb"
@@ -20,6 +21,8 @@ func main() {
 	c := calculatorpb.NewCalculatorServiceClient(cc)
 
 	doSum(c)
+
+	doPrimeFactorization(c)
 }
 
 func doSum(c calculatorpb.CalculatorServiceClient) {
@@ -34,4 +37,32 @@ func doSum(c calculatorpb.CalculatorServiceClient) {
 		log.Fatalf("Failed to send request: %v", err)
 	}
 	log.Printf("Result of addition: %v", res.SumResult)
+}
+
+func doPrimeFactorization(c calculatorpb.CalculatorServiceClient) {
+	fmt.Println("Starting prime factorization")
+	req := &calculatorpb.PrimeNumberDecompositionRequest{
+		Number: 13571947292733,
+	}
+
+	// the output of calling the rpc is the stream from which you read the response using Recv()
+	stream, err := c.PrimeNumberDecomposition(context.Background(), req)
+
+	if err != nil {
+		log.Fatalf("error calling decomposition: %v", err)
+	}
+
+	for {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+			// end of stream
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("Error while reading stream: %v", err)
+		}
+
+		log.Printf("Prime Factor: %v", msg.GetPrimeFactor())
+	}
 }
--- calculator/calculator_server/server.go
+++ calculator/calculator_server/server.go
@@ -20,6 +20,35 @@ func (*server) Sum(ctx context.Context, req *calculatorpb.SumRequest) (*calculat
 	return &resp, nil
 }
 
+// The argument to the rpc is the request and the stream.
+func (*server) PrimeNumberDecomposition(req *calculatorpb.PrimeNumberDecompositionRequest, stream calculatorpb.CalculatorService_PrimeNumberDecompositionServer) error {
+	log.Printf("Received request: %v\n", req)
+
+	number := req.GetNumber()
+	//  now run the algorithm to find the prime factors
+	// start with divisior as 2. keep dividing by divisor as long as remainder is 0.
+	// once remainder is no longer 0, incremeent the divisor and repeat the division
+
+	divisor := uint64(2)
+
+	for number > 1 {
+		// log.Printf("Number: %v \t Divisor: %d\n", number, divisor)
+		if number%divisor == 0 {
+			// divisor is a factor of the number, send it out
+			stream.Send(&calculatorpb.PrimeNumberDecompositionResponse{
+				PrimeFactor: divisor,
+			})
+			// divide the number by divisor and repeat cycle
+			number /= divisor
+		} else {
+			// divisor is not a factor of the number, increment the divisor
+			divisor++
+		}
+	}
+	// all done
+	return nil
+}
+
 func main() {
 	log.Println("Starting server")
 
